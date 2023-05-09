41410803484acb09cf34b0b543169bdb9c4644e5
Sriram Rokkam <sriramrokkam@gmail.com>
2023-05-09 21:08:15 +0530

    final

--- 07-speech/Python/speaking-clock/Text2Speech.py
+++ 07-speech/Python/speaking-clock/Text2Speech.py
@@ -1,12 +1,14 @@
 from dotenv import load_dotenv
 import azure.cognitiveservices.speech as speechsdk
-speech_key="37d48b0c67b74a87b7eb5ad2b788c335"
-service_region="eastasia"
+speech_key = "37d48b0c67b74a87b7eb5ad2b788c335"
+service_region = "eastasia"
+
 
 def speech_synthesis_with_auto_language_detection_to_speaker(text):
     """performs speech synthesis to the default speaker with auto language detection
        Note: this is a preview feature, which might be updated in future versions."""
-    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+    speech_config = speechsdk.SpeechConfig(
+        subscription=speech_key, region=service_region)
 
     # create the auto detection language configuration without specific languages
     auto_detect_source_language_config = speechsdk.languageconfig.AutoDetectSourceLanguageConfig()
@@ -16,10 +18,12 @@ def speech_synthesis_with_auto_language_detection_to_speaker(text):
         speech_config=speech_config, auto_detect_source_language_config=auto_detect_source_language_config)
 
     result = speech_synthesizer.speak_text_async(text).get()
-        # Check result
+    # Check result
     if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-            print("Speech synthesized to speaker for text [{}]".format(text))
-            stream = speechsdk.AudioDataStream(result)
-            stream.save_to_wav_file(r"C:\Users\user\Desktop\outputfff.wav")
+        print("Speech synthesized to speaker for text [{}]".format(text))
+        stream = speechsdk.AudioDataStream(result)
+        stream.save_to_wav_file(r"C:\Users\user\Desktop\outputfff.wav")
 
-speech_synthesis_with_auto_language_detection_to_speaker("హలో గౌతమి. నేను ఒక AI రోబోట్! నా పేరు శ్రీరామ్! మీరు ఏలా ఉన్నారు ?")
+
+read_text = input("Enter Text to Read :")
+speech_synthesis_with_auto_language_detection_to_speaker(read_text)
