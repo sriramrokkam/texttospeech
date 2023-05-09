4d5e08fae027442cc932ba11d0767d822608640a
Sriram Rokkam <sriramrokkam@gmail.com>
2023-05-09 20:48:49 +0530

    Cognitive AI

--- 07-speech/Python/speaking-clock/speech_recognition.py
+++ 07-speech/Python/speaking-clock/speech_recognition.py
@@ -1,26 +1,25 @@
-import os
+from dotenv import load_dotenv
 import azure.cognitiveservices.speech as speechsdk
+speech_key="37d48b0c67b74a87b7eb5ad2b788c335"
+service_region="eastasia"
 
-def recognize_from_microphone():
-    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-    speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
-    speech_config.speech_recognition_language="en-US"
+def speech_synthesis_with_auto_language_detection_to_speaker(text):
+    """performs speech synthesis to the default speaker with auto language detection
+       Note: this is a preview feature, which might be updated in future versions."""
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
 
-    audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
-    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+    # create the auto detection language configuration without specific languages
+    auto_detect_source_language_config = speechsdk.languageconfig.AutoDetectSourceLanguageConfig()
 
-    print("Speak into your microphone.")
-    speech_recognition_result = speech_recognizer.recognize_once_async().get()
+    # Creates a speech synthesizer using the default speaker as audio output.
+    speech_synthesizer = speechsdk.SpeechSynthesizer(
+        speech_config=speech_config, auto_detect_source_language_config=auto_detect_source_language_config)
 
-    if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        print("Recognized: {}".format(speech_recognition_result.text))
-    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
-    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = speech_recognition_result.cancellation_details
-        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            print("Error details: {}".format(cancellation_details.error_details))
-            print("Did you set the speech resource key and region values?")
+    result = speech_synthesizer.speak_text_async(text).get()
+        # Check result
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+            print("Speech synthesized to speaker for text [{}]".format(text))
+            stream = speechsdk.AudioDataStream(result)
+            stream.save_to_wav_file(r"C:\Users\user\Desktop\outputfff.wav")
 
-recognize_from_microphone()
+speech_synthesis_with_auto_language_detection_to_speaker("तू कसा आहेस ")
