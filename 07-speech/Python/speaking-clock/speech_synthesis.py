6469519eb8b3bc9c0896d7de3bc594fd1863a68b
Sriram Rokkam <sriramrokkam@gmail.com>
2023-05-13 17:49:03 +0530

    commit

--- 07-speech/Python/speaking-clock/speech_synthesis.py
+++ 07-speech/Python/speaking-clock/speech_synthesis.py
@@ -1,24 +1,30 @@
+import os
 import azure.cognitiveservices.speech as speechsdk
-speech_key="37d48b0c67b74a87b7eb5ad2b788c335"
-service_region="eastasia"
+speech_key = "37d48b0c67b74a87b7eb5ad2b788c335"
+service_region = "eastasia"
 
-def speech_synthesis_with_auto_language_detection_to_speaker(text):
-    """performs speech synthesis to the default speaker with auto language detection
-       Note: this is a preview feature, which might be updated in future versions."""
-    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
 
-    # create the auto detection language configuration without specific languages
-    auto_detect_source_language_config = speechsdk.languageconfig.AutoDetectSourceLanguageConfig()
+# This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
+audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
 
-    # Creates a speech synthesizer using the default speaker as audio output.
-    speech_synthesizer = speechsdk.SpeechSynthesizer(
-        speech_config=speech_config, auto_detect_source_language_config=auto_detect_source_language_config)
+# The language of the voice that speaks.
+speech_config.speech_synthesis_voice_name='en-US-JennyNeural'
 
-    result = speech_synthesizer.speak_text_async(text).get()
-        # Check result
-    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-            print("Speech synthesized to speaker for text [{}]".format(text))
-            stream = speechsdk.AudioDataStream(result)
-            stream.save_to_wav_file(r"C:\Users\user\Desktop\outputfff.wav")
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
-            speech_synthesis_with_auto_language_detection_to_speaker("हैलो मैं श्रीराम हूं, मैं एक आर्टिफिशियल इंटेलिजेंट रोबोट हूं!")
+# Get text from the console and synthesize to the default speaker.
+print("Enter some text that you want to speak >")
+text = input()
+
+speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
+
+if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    print("Speech synthesized for text [{}]".format(text))
+elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = speech_synthesis_result.cancellation_details
+    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        if cancellation_details.error_details:
+            print("Error details: {}".format(cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
